2114164f77e52b17a40a27216b32e95d18a293d7
GabiFerraz <ferrazmgabi@gmail.com>
2025-01-04 10:35:40 -0300

    Iniciando o projeto

---
+++ src/main/java/com/api/ParkingMeter/ParkingMeterSolutionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.api.ParkingMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingMeterSolutionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingMeterSolutionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ParkingMeter/ParkingMeterSolutionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ParkingMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingMeterSolutionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
